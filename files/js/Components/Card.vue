0918d0819eef5900e909ec1f8cce9482c5b15ca2
Wojciech Przybyła <contact@wpww.pl>
2025-04-14 20:00:18 +0200

    🪟 refined card component

--- files/js/Components/Card.vue
+++ files/js/Components/Card.vue
@@ -1,5 +1,25 @@
+<script setup lang="ts">
+import Icon from './Icon.vue';
+
+const props = defineProps<{
+    title?: string,
+    icon?: string,
+}>()
+</script>
+
 <template>
     <div class="card">
+        <div class="title-wrapper" v-if="props.title">
+            <h2>
+                <Icon v-if="props.icon" :icon="props.icon" />
+                {{ props.title }}
+            </h2>
+
+            <div class="buttons" v-if="$slots.buttons">
+                <slot name="buttons" />
+            </div>
+        </div>
+
         <slot />
     </div>
 </template>
