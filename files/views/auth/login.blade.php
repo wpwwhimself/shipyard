d31bdfbac8ac0c0594407de1f89a3551912a752a
Wojciech Przybyła <contact@wpww.pl>
2025-08-25 11:48:29 +0200

    💄 updated input look

--- files/views/auth/login.blade.php
+++ files/views/auth/login.blade.php
@@ -2,6 +2,29 @@
 
 @section("content")
 
-asdf
+<form action="{{ route('process-login') }}" method="post">
+    @csrf
+    
+    <x-shipyard.ui.input
+        name="email"
+        type="email"
+        label="Email"
+        icon="user"
+        required
+    />
+    <x-shipyard.ui.input
+        name="password"
+        type="password"
+        label="Hasło"
+        icon="key"
+        required
+    />
+
+    <x-shipyard.ui.button
+        icon="right-to-bracket"
+        label="Zaloguj się"
+        action="submit"
+    />
+</form>
 
 @endsection
