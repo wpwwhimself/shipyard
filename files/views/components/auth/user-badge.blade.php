744b23a3c7d2f79fbaef96fd9c9dae99e619439d
Wojciech Przybyła <contact@wpww.pl>
2025-08-25 14:13:12 +0200

    🏗️ changed configuration for user model

--- files/views/components/auth/user-badge.blade.php
+++ files/views/components/auth/user-badge.blade.php
@@ -1,5 +1,16 @@
 @auth
-
+<x-shipyard.ui.button
+    icon="user"
+    label="Profil"
+    :action="route('profile')"
+/>
+@if (Auth::user()->hasRole("administrator"))
+<x-shipyard.ui.button
+    icon="right-from-bracket"
+    pop="Wyloguj"
+    :action="route('logout')"
+    class="danger"
+/>
 
 @else
 <x-shipyard.ui.button
