285c9045f8765389b67b4974de4435d8b1f02ea4
Wojciech Przybyła <contact@wpww.pl>
2025-09-02 14:30:36 +0200

    📜 updated copyright icons

--- files/views/components/app/app-badge.blade.php
+++ files/views/components/app/app-badge.blade.php
@@ -4,10 +4,11 @@
     <div role="details">
         <h2 role="app-name"><a href="/">{{ setting("app_name") }}</a></h2>
         <span role="copyright">
-            <a href="https://creativecommons.org/licenses/by-sa/3.0/pl/">
-                <i class="fa-brands fa-creative-commons" aria-hidden="true"></i>
-                <i class="fa-brands fa-creative-commons-by" aria-hidden="true"></i>
-                <i class="fa-brands fa-creative-commons-sa" aria-hidden="true"></i>
+            <a href="https://creativecommons.org/licenses/by-sa/4.0/deed.pl">
+                <img class="icon" src="https://creativecommons.org/wp-content/themes/vocabulary-theme/vocabulary/svg/cc/icons/cc-icons.svg#cc-logo" alt="cc">
+                <img class="icon" src="https://creativecommons.org/wp-content/themes/vocabulary-theme/vocabulary/svg/cc/icons/cc-icons.svg#cc-by" alt="by">
+                <img class="icon" src="https://creativecommons.org/wp-content/themes/vocabulary-theme/vocabulary/svg/cc/icons/cc-icons.svg#cc-sa" alt="sa">
+                <span>CC BY-SA 4.0</span>
             </a>
             {{ app_lifetime() }}
             <span role="author"><x-shipyard.app.wpww-tag /></span>
