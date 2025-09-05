e6d565d27256eb4aadee2156e8875bb8476c139e
Wojciech Przybyła <contact@wpww.pl>
2025-09-05 23:59:58 +0200

    💄 updated theme-responsive custom icons

--- files/views/components/app/app-badge.blade.php
+++ files/views/components/app/app-badge.blade.php
@@ -5,9 +5,9 @@
         <h2 role="app-name"><a href="/">{{ setting("app_name") }}</a></h2>
         <span role="copyright">
             <a href="https://creativecommons.org/licenses/by-sa/4.0/deed.pl">
-                <img class="icon" src="https://creativecommons.org/wp-content/themes/vocabulary-theme/vocabulary/svg/cc/icons/cc-icons.svg#cc-logo" alt="cc">
-                <img class="icon" src="https://creativecommons.org/wp-content/themes/vocabulary-theme/vocabulary/svg/cc/icons/cc-icons.svg#cc-by" alt="by">
-                <img class="icon" src="https://creativecommons.org/wp-content/themes/vocabulary-theme/vocabulary/svg/cc/icons/cc-icons.svg#cc-sa" alt="sa">
+                <img class="icon invert-when-dark" src="https://creativecommons.org/wp-content/themes/vocabulary-theme/vocabulary/svg/cc/icons/cc-icons.svg#cc-logo" alt="cc">
+                <img class="icon invert-when-dark" src="https://creativecommons.org/wp-content/themes/vocabulary-theme/vocabulary/svg/cc/icons/cc-icons.svg#cc-by" alt="by">
+                <img class="icon invert-when-dark" src="https://creativecommons.org/wp-content/themes/vocabulary-theme/vocabulary/svg/cc/icons/cc-icons.svg#cc-sa" alt="sa">
                 <span>CC BY-SA 4.0</span>
             </a>
             {{ app_lifetime() }}
