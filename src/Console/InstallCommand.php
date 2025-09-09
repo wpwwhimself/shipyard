95f2a1874c79db95fa484422544652804e869756
Wojciech Przybyła <contact@wpww.pl>
2025-09-09 12:28:21 +0200

    🩹 updated installation - it now should work out of the box

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -121,8 +121,8 @@ class InstallCommand extends Command
         $this->info("✅ Shipyard is ready!");
 
         $this->comment("Things to do now:");
-        $this->comment("> in your `routes/web.php` add the following: \n\t require __DIR__.'/Shipyard/shipyard.php';");
-        $this->comment("> in your `routes/console.php` add the following: \n\t require __DIR__.'/Shipyard/shipyard_schedule.php';");
+        $this->comment("> in your `routes/web.php` add the following: \n\t if (file_exists(__DIR__.'/Shipyard/shipyard.php')) require __DIR__.'/Shipyard/shipyard.php';");
+        $this->comment("> in your `routes/console.php` add the following: \n\t if (file_exists(__DIR__.'/Shipyard/shipyard_schedule.php')) require __DIR__.'/Shipyard/shipyard_schedule.php';");
         $this->comment("> clear your `resources/css/app.css` file - it may overwrite themes");
 
         return Command::SUCCESS;
