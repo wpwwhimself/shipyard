282ad844803b2644f47fe0801ab0e799692a4b8d
Wojciech Przybyła <contact@wpww.pl>
2025-09-17 19:33:00 +0200

    🪟 fixed presentation for multi-relations

--- files/views/components/app/model/card.blade.php
+++ files/views/components/app/model/card.blade.php
@@ -28,7 +28,10 @@
             <span {{ Popper::pop($data['field_label'] ?? model($connection_name)::META['label']) }}>
                 <x-shipyard.app.icon :name="model_icon($connection_name)" />
             </span>
-            <span>{!! $model->{$connection_name} !!}</span>
+            <span>{!! $data["mode"] === "many"
+                ? $model->{$connection_name}->map(fn ($i) => $i->__toString())->join(", ")
+                : $model->{$connection_name} !!}
+            </span>
         </span>
         @endforeach
     </div>
