09185f8fa0d8c72451fc027eb1ff4fd2c48e7919
Wojciech Przybyła <contact@wpww.pl>
2025-11-13 23:14:43 +0100

    ✨ added fields preview

---
+++ files/views/components/app/model/fields-preview.blade.php
@@ -0,0 +1,19 @@
+@props([
+    "fields" => [],
+    "model",
+])
+
+@php
+$data = $model::getFields();
+@endphp
+
+<div>
+    @foreach ($fields as $field_name)
+    <div>
+        <span {{ Popper::pop($data[$field_name]["label"]) }}>
+            <x-shipyard.app.icon :name="$data[$field_name]['icon']" />
+        </span>
+        <span>{!! $model->{$field_name} ?? "—" !!}</span>
+    </div>
+    @endforeach
+</div>
