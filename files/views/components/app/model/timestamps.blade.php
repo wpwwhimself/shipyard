be7f4280e5fac56a583eb93023f0afbe03d39bc7
Wojciech Przybyła <contact@wpww.pl>
2025-11-17 23:52:23 +0100

    🩹 allowed model timestamps to be visible even for models without userstamps

--- files/views/components/app/model/timestamps.blade.php
+++ files/views/components/app/model/timestamps.blade.php
@@ -2,23 +2,27 @@
     "model",
 ])
 
-@if ($model && isset($model->created_by))
+@if ($model && isset($model->created_at))
 <div class="ghost">
     {{-- todo przerobić to i podobne na komponenty --}}
     <x-shipyard.app.icon-label-value
         icon="account-plus"
-        label="Twórca"
+        label="Utworzono"
     >
+        @isset ($model->created_by)
         {{ $model->creator->name }},
+        @endisset
         <span {{ Popper::pop($model->created_at ?? "") }}>{{ $model->created_at?->diffForHumans() }}</span>
     </x-shipyard.app.icon-label-value>
 
-    @if ($model->created_at != $model->updated_at)
+    @if (isset($model->updated_at) && $model->created_at != $model->updated_at)
     <x-shipyard.app.icon-label-value
         icon="account-edit"
         label="Ostatnia edycja"
     >
+        @isset ($model->updated_by)
         {{ $model->editor->name }},
+        @endisset
         <span {{ Popper::pop($model->updated_at ?? "") }}>{{ $model->updated_at?->diffForHumans() }}</span>
     </x-shipyard.app.icon-label-value>
     @endif
