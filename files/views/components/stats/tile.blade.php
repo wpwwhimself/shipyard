676573110eff4c2169398a3b906acf659e563fd3
Wojciech Przybyła <contact@wpww.pl>
2025-11-19 23:11:23 +0100

    🪗 stat tiles now can show extra information

--- files/views/components/stats/tile.blade.php
+++ files/views/components/stats/tile.blade.php
@@ -1,8 +1,31 @@
 @props([
     "label",
+    "value" => null,
+    "percentageOf" => null,
+    "comparedTo" => null,
 ])
 
 <div class="stat-tile">
     <span role="label subtitle">{{ $label }}</span>
-    <span role="value">{{ $slot }}</span>
+    <span role="value">
+        {{ $value ?? $slot }}
+
+        @if ($comparedTo !== null)
+        @php
+        $diff = $value - $comparedTo;
+        @endphp
+        <small @class(["accent hot" => $diff > 0, "accent cold" => $diff < 0])>
+            {{ $diff > 0 ? "+$diff" : $diff }}
+        </small>
+        @endif
+
+        @if ($percentageOf !== null)
+        @php
+        $perc = round($value / $percentageOf * 100);
+        @endphp
+        <small>
+            ({{ $perc }}%)
+        </small>
+        @endif
+    </span>
 </div>
