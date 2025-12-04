3accf336fe78bb08e3313e7234838a947f740593
Wojciech Przybyła <contact@wpww.pl>
2025-12-04 23:40:08 +0100

    🪗 listing edit button can now be customized

--- files/views/pages/admin/model/list.blade.php
+++ files/views/pages/admin/model/list.blade.php
@@ -124,14 +124,18 @@
     @endif
 
     @forelse ($data as $item)
-    <x-shipyard.app.model.tile :model="$item" class="{{ $item->is_uneditable ? 'ghost' : null }}">
+    <x-shipyard.app.model.tile :model="$item" @class(["ghost" => $item->is_uneditable ?? false])>
         <x-slot:actions>
             @unless ($item->is_uneditable)
-            <x-shipyard.ui.button
-                icon="pencil"
-                label="Edytuj"
-                :action="route('admin.model.edit', ['model' => $scope, 'id' => $item->getKey()])"
-            />
+                @if ($item->model_edit_button)
+                {!! $item->model_edit_button !!}
+                @else
+                <x-shipyard.ui.button
+                    icon="pencil"
+                    label="Edytuj"
+                    :action="route('admin.model.edit', ['model' => $scope, 'id' => $item->getKey()])"
+                />
+                @endif
             @endunless
         </x-slot:actions>
     </x-shipyard.app.model.tile>
