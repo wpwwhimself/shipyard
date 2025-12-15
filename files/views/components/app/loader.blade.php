d01dfcf469956deb96e20b2b2225e5cd3b166047
Wojciech Przybyła <contact@wpww.pl>
2025-12-15 21:28:57 +0100

    ✨ added horizontal loader

--- files/views/components/app/loader.blade.php
+++ files/views/components/app/loader.blade.php
@@ -1,4 +1,17 @@
-<div class="loader hidden">
+@props([
+    "horizontal" => false,
+])
+
+<div @class([
+    "loader",
+    "hidden",
+    "horizontal" => $horizontal
+])>
     <x-shipyard.app.logo :clickable="false" />
+
+    @if ($horizontal)
+    <span class="icon"></span>
+    @else
     <x-shipyard.app.icon name="loading" />
+    @endif
 </div>
