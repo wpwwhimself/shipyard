c97aa70d99b20bc88ae21749dd490b33750738da
Wojciech Przybyła <contact@wpww.pl>
2025-12-17 13:32:39 +0100

    💫 more concise app badge

--- files/views/components/app/app-badge.blade.php
+++ files/views/components/app/app-badge.blade.php
@@ -4,14 +4,15 @@
     <div role="details">
         <h2 role="app-name"><a href="/">{{ setting("app_name") }}</a></h2>
         <span role="copyright">
-            <a href="https://creativecommons.org/licenses/by-sa/4.0/deed.pl">
+            <a href="https://creativecommons.org/licenses/by-sa/4.0/deed.pl" @popper(CC BY-SA 4.0)>
                 <img class="icon invert-when-dark" src="https://creativecommons.org/wp-content/themes/vocabulary-theme/vocabulary/svg/cc/icons/cc-icons.svg#cc-logo" alt="cc">
                 <img class="icon invert-when-dark" src="https://creativecommons.org/wp-content/themes/vocabulary-theme/vocabulary/svg/cc/icons/cc-icons.svg#cc-by" alt="by">
                 <img class="icon invert-when-dark" src="https://creativecommons.org/wp-content/themes/vocabulary-theme/vocabulary/svg/cc/icons/cc-icons.svg#cc-sa" alt="sa">
-                <span>CC BY-SA 4.0</span>
             </a>
             {{ app_lifetime() }}
-            <span role="author"><x-shipyard.app.wpww-tag /></span>
+            <span role="author" @popper(Projekt i wykonanie)>
+                <x-shipyard.app.wpww-tag />
+            </span>
         </span>
     </div>
 </div>
