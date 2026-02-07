ce96a2fd7d5e93209f500049f6a1427044614192
Wojciech Przybyła <contact@wpww.pl>
2026-02-07 21:06:23 +0100

    🪗 added more icon options to headings

--- files/views/components/app/h.blade.php
+++ files/views/components/app/h.blade.php
@@ -1,9 +1,11 @@
 @props([
     "lvl" => 1,
     "icon" => null,
+    "iconMode" => null,
+    "iconData" => null,
 ])
 
 <h{{ $lvl }} {{ $attributes }}>
-    @if ($icon) <x-shipyard.app.icon :name="$icon" /> @endif
+    @if ($icon) <x-shipyard.app.icon :name="$icon" :mode="$iconMode" :data="$iconData" /> @endif
     {{ $slot }}
 </h{{ $lvl }}>
