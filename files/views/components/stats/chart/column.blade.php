7a76ee602c0a0e36ef59ea73dd5afd36af164971
Wojciech Przybyła <contact@wpww.pl>
2026-05-09 22:46:13 +0200

    🪗 charts now can display more nuanced data

--- files/views/components/stats/chart/column.blade.php
+++ files/views/components/stats/chart/column.blade.php
@@ -2,11 +2,13 @@
     "title" => null,
     "subtitle" => null,
     "icon" => null,
-    "data" => [],
+    "data" => [], // każdy element musi mieć "label" (string) i "value" (liczba). "value_label" wymusza wyświetlanie wartości po najechaniu
+    "mode" => "normal", // normal (bez zmian) | monetary (formatowanie walutowe) | percentage (obliczanie procentów)
 ])
 
 @php
-$maxValue = $data->max("value");
+$maxValue = collect($data)->max("value");
+$sumValue = collect($data)->sum("value");
 @endphp
 
 <x-shipyard.stats.chart.wrapper
@@ -22,13 +24,28 @@ $maxValue = $data->max("value");
     @endisset
 
     @foreach ($data as $d)
+    @php
+    $value_pretty = $d["value"];
+    if ($mode == "percentage") {
+        $value_pretty = round($d["value"] / $sumValue * 100, 1) . "%";
+        $d["value_label"] ??= $d["value"];
+    } else if ($mode == "monetary") {
+        $value_pretty = number_format(
+            round($d["value"], 2),
+            $mode == "monetary" ? 2 : 0,
+            ",",
+            " "
+        );
+    }
+
+    @endphp
     <div class="value-wrapper">
         <div class="value" style="height: {{ $d['value'] / $maxValue * 100 }}%;"
             @if ($d["value_label"] ?? null)
             {{ Popper::pop($d["value_label"]) }}
             @endif
         >
-            <span class="value-label">{{ $d["value"] }}</span>
+            <span class="value-label">{{ $value_pretty }}</span>
         </div>
     </div>
     <div class="label">
