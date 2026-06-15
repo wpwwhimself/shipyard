4e18f98c03473edd44b244a192cab768497ac132
Wojciech Przybyła <contact@wpww.pl>
2026-06-15 19:11:19 +0200

    🚑 fixed improperly named icon

--- files/views/components/app/model/field-value.blade.php
+++ files/views/components/app/model/field-value.blade.php
@@ -3,10 +3,10 @@
     "field",
 ])
 
-<x-shipyard.icon-label-value
+<x-shipyard.app.icon-label-value
     :icon="$model::getFields()[$field]['icon']"
     :label="$model::getFields()[$field]['label']"
     class="field-value"
 >
     {{ $slot ?? $model->{$field} }}
-</x-shipyard.icon-label-value>
+</x-shipyard.app.icon-label-value>
