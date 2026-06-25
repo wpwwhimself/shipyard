58876726cdebdd78357d7a55052ac26a7d6989a7
Wojciech Przybyła <contact@wpww.pl>
2026-06-25 23:25:05 +0200

    💄 stylized spellbook

--- files/views/pages/docs/spellbook.blade.php
+++ files/views/pages/docs/spellbook.blade.php
@@ -7,11 +7,13 @@
 <x-shipyard.app.card
     title="Lista zaklęć"
     icon="wizard-hat"
+    title-lvl="2"
+    role="doc-contents"
     @class(["stagger" => setting("animations_mode") >= 1])
 >
     <div @class(["flex", "down", "stagger-contents" => setting("animations_mode") >= 2])>
     @foreach ($spells as $name => $props)
-    <x-shipyard.app.h lvl="3">{{ $props["route"] }}</x-shipyard.app.h>
+    <x-shipyard.app.h lvl="2">{{ $props["route"] }}</x-shipyard.app.h>
     {!! \Illuminate\Mail\Markdown::parse(preg_replace("/ +/", " ", $props["description"])) !!}
     @endforeach
     </div>
