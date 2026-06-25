7f57dec666f05904220036d51e60bcd989bf85b2
Wojciech Przybyła <contact@wpww.pl>
2026-06-25 23:29:42 +0200

    💫 final animations in docs

--- files/views/pages/docs/view.blade.php
+++ files/views/pages/docs/view.blade.php
@@ -18,11 +18,15 @@
     <div role="doc-contents"
         @class([
             "flex down",
-            "stagger-contents" => setting("animations_mode") >= 2,
+            "stagger-contents" => setting("animations_mode") >= 1,
         ])
     >
         @foreach ($doc as $heading => $content)
-        <x-shipyard.app.card :title="$heading" title-lvl="2" :icon="$icon ?? 'book-information-variant'">
+        <x-shipyard.app.card :title="$heading"
+            title-lvl="2"
+            :icon="$icon ?? 'book-information-variant'"
+            :inner-class="setting('animations_mode') >= 2 ? 'stagger-contents' : null"
+        >
             {!! \Illuminate\Mail\Markdown::parse($content) !!}
         </x-shipyard.app.card>
         @endforeach
