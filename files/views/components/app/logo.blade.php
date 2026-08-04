019054a61b1018f2ebfef8ac5e199311cc90f4b2
Wojciech Przybyła <contact@wpww.pl>
2026-08-04 23:16:24 +0200

    💄 updated treating light-dark logos when they aren't defined

--- files/views/components/app/logo.blade.php
+++ files/views/components/app/logo.blade.php
@@ -12,22 +12,31 @@
         srcset="{{ asset(setting("app_logo_path")) }}"
         media="(prefers-color-scheme: light)"
     />
+    @if (setting("app_logo_dark_path") !== null)
     <source
         @class(["logo"])
         srcset="{{ asset(setting("app_logo_dark_path")) }}"
         media="(prefers-color-scheme: dark)"
     />
     @endif
+    @endif
+
     <img
-        @class(["logo"])
+        @class([
+            "logo",
+            "light",
+            "dark" => setting("app_logo_dark_path") === null,
+        ])
         src="{{ asset(setting("app_logo_path")) }}"
         alt="{{ setting("app_name") }}"
     >
+    @if (setting("app_logo_dark_path") !== null)
     <img
         @class(["logo", "dark"])
         src="{{ asset(setting("app_logo_dark_path")) }}"
         alt="{{ setting("app_name") }}"
     >
+    @endif
 </picture>
 
 @if ($clickable) </a> @endif
